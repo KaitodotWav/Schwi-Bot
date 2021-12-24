b3f0d9210f4b4660060977f3fb70c4a14741ead4
KaitodotWav <78613734+KaitodotWav@users.noreply.github.com>
2021-12-24 21:43:53 +0800

    Add root files

--- README.md
+++ README.md
@@ -1,1 +1,53 @@
-# Schwi-Bot2.0
+# Schwi-Bot
+Bot for discord
+
+-------------------------------------------------------
+info 
+-------------------------------------------------------
+Python programs:
+
+--『Schwi.py』= main program
+
+--『CMD.py』= for extension of Schwi.py, used to extend bot commands without writing very long code on main program
+
+--『mcsrvstat.py』= module i created to fetch data about minecraft servers using mcsrvstat API
+
+
+Folders:
+
+--  /cogs = bot commands and functions
+
+--  /dumps = dumping folder of servers meta data
+
+--  /KaitoUWU = all packages created for schwii
+
+
+
+Data files:
+
+--  Properties.ini = Bot Settings
+
+--  mcsrvMonitor.json = Meta data of servers being monitor
+
+
+-------------------------------------------------------
+Note:
+
+this bot is currently being coded base on the first bot "Poyoyo bot"
+
+still not done but already functional
+
+a faster, less buggy and improved discord bot
+
+-------------------------------------------------------
+Branches:
+My laptop is broken due to this reason i cannot delete unused branches
+on my repo so i have alot of them
+PAIN
+ill delete them if i can for now just deal with them
+
+--Main = main release and being used by the Schwi bot
+
+--Pub = beta release and being used by Poyoyo bot
+
+
---
+++ Schwi.py
@@ -0,0 +1,104 @@
+#Schwi bot created by Kaito
+
+#imports
+import discord, time, sys, configparser, requests, json, os
+from discord.ext import commands
+from KaitoUWU import CMD
+from KaitoUWU import BotUtils
+
+config = configparser.ConfigParser()
+config.read("Properties.ini")
+errors = []
+
+#Auth
+Token = str(config["Schwi"]["Token"])
+
+client = commands.Bot(command_prefix = str(config["Schwi"]["Prefix"]))
+
+"""
+#Instance
+def ShowWarning(TITLE, MESSAGE):
+    root = Tk()
+    root.withdraw()
+    TKmsg.showwarning(title=TITLE, message=MESSAGE)
+from tendo import singleton
+
+try:
+    me = singleton.SingleInstance()
+except:
+    warnThread = Thread(target=ShowWarning, args=("Error: Multiple Instance", "Main program is already running."))
+    warnThread.start()
+    WriteF("Error: Reject launch\nMain program is already running", "MainReport.txt")
+    print("done")
+    sys.exit(-1)
+else:
+    pass
+"""
+
+#Commands
+CMD.Process(client)
+
+@client.command()
+async def reload(ctx, extension):
+    client.unload_extension(f"cogs.{extension}")
+    client.load_extension(f"cogs.{extension}")
+    await ctx.send(f"{extension} has been reloaded")
+
+@client.command()
+async def load(ctx, extension):
+    try:
+        client.load_extension(f"cogs.{extension}")
+        await ctx.send(f"{extension} has been loaded")
+    except Exception as e:
+        erremb = discord.Embed(title="Error!", description=f"{e}", color=0xFF0000)
+        await ctx.send(embed=erremb)
+        
+@client.command()
+async def unload(ctx, extension):
+    try:
+        client.unload_extension(f"cogs.{extension}")
+        await ctx.send(f"{extension} has been unloaded")
+    except Exception as e:
+        erremb = discord.Embed(title="Error!", description=f"{e}", color=0xFF0000)
+        await ctx.send(embed=erremb)
+    
+@client.event
+async def on_ready():
+    report = client.get_channel(int(config["Notifs"]["Reports"]))
+    
+    try:
+        import socket
+        host = socket.gethostname()
+    except Exception as e:
+        print(e)
+        errors.append((type(e), e))
+        host = "unknown"
+    finally:
+        on_emb = discord.Embed(title=f"{client.user} is now online", description=f"Host:{host}", color=0x00FF00)
+        for i in errors:
+            print(i)
+            emb = BotUtils.EMBEDS(Type="error", title="Error!", description="while starting the bot.")
+            sendE = emb.get()
+            sendE.add_field(name=str(i[0]), value=str(i[1]))
+            await report.send(embed=sendE)
+        print(f"{client.user} is now online on host:{host}")
+        await report.send(embed=on_emb)
+
+#run
+for filename in os.listdir("./cogs"):
+    if filename.endswith(".py"):
+        if filename.startswith("_"):
+            pass
+        else:
+            try:
+                client.load_extension(f"cogs.{filename[:-3]}")
+            except Exception as e:
+                errors.append((type(e), e))
+                print(f"Error!: {e}")
+try:
+    client.run(Token)
+except Exception as e:
+    if str(e) == "Cannot connect to host discord.com:443 ssl:default [getaddrinfo failed]":
+        print("Error! no internet.")
+    else:
+        print(e)
