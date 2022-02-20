2d2c7f423931b7904afd87dd114826ac76a7e692
KaitodotWav <78613734+KaitodotWav@users.noreply.github.com>
2022-02-20 16:45:51 +0800

    link key test

--- cogs/NSFW.py
+++ cogs/NSFW.py
@@ -9,7 +9,7 @@ class Yabai(commands.Cog):
     def buildEMB(self, Dict):
         emb = discord.Embed(title=Dict["title"], description="Score:{}".format(Dict["scores"]))
         emb.set_image(url=Dict["url"])
-        emb.add_field(name="nsfw", value=Dict["nsfw"])
+        emb.add_field(name="link", value=Dict["url"])
         return emb
 
     async def dm(user, item):
@@ -18,7 +18,7 @@ class Yabai(commands.Cog):
     @commands.command()
     async def culture(self, ctx, Type, index="random", *Options):
         if len(Options) == 0:
-            Options = ("top", 1)
+            Options = ("top", 10)
         if str(Type).startswith("r/"):
             if index == "random":
                 index = "lewds"
