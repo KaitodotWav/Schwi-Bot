5dae62c9d2e81b46e29eabfa84bba105ca0cb38b
KaitodotWav <78613734+KaitodotWav@users.noreply.github.com>
2022-02-21 20:58:21 +0800

    removed logger again

--- Schwi.py
+++ Schwi.py
@@ -1,22 +1,10 @@
 #Schwi bot created by Kaito
 
 #imports
-import discord, time, sys, configparser, requests, json, os 
-
-class Tee:
-    def write(self, *args, **kwargs):
-        self.out1.write(*args, **kwargs)
-        self.out2.write(*args, **kwargs)
-    def __init__(self, out1, out2):
-        self.out1 = out1
-        self.out2 = out2
+import discord, time, sys, configparser, requests, json, os
 
 with open("Data\\logs.txt", "a") as log:
     print("bot logger is connected", file=log)
-    sys.stdout = Tee(log, sys.stdout)
-
-print("hello")
-
 
 from KaitoUWU import BotUtils
 
