06cad234789a957533f2671985fd9c0870746959
KaitodotWav <78613734+KaitodotWav@users.noreply.github.com>
2022-02-25 03:15:15 +0800

    fix typo

--- cogs/twitter.py
+++ cogs/twitter.py
@@ -24,7 +24,7 @@ class Twitter(commands.Cog):
     @commands.command()
     async def debug(self, ctx):
         try:
-            await ctx.send(self.me.Response('data'))
+            await ctx.send(self.me.Response.data)
         except Exception as e:
             await ctx.send(e)
 
