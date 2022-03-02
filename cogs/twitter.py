f8fa624f808c1ab84033d99f4d1db542b07d0cd4
KaitodotWav <78613734+KaitodotWav@users.noreply.github.com>
2022-03-02 19:11:14 +0800

    bring back tweepy v2

--- cogs/twitter.py
+++ cogs/twitter.py
@@ -15,19 +15,17 @@ asecret = str(tweetC['secret'])
 ckey = str(tweetC['con_key'])
 csecret = str(tweetC['con_secret'])
 bearer = str(tweetC['bearer'])
-
+"""
 auth = tweepy.OAuthHandler(ckey, csecret)
 auth.set_access_token(akey, asecret)
-
 """
 clientA = tweepy.Client(
-    #bearer_token=
-    consumer_key=,
-    consumer_secret=,
-    access_token=,
-    access_token_secret=
+    bearer_token=bearer,
+    consumer_key=ckey,
+    consumer_secret=csecret,
+    access_token=akey,
+    access_token_secret=asecret
 )
-"""
 
 client2 = tweepy.API(auth)
 
@@ -35,15 +33,15 @@ class Twitter(commands.Cog):
     def __init__(self, client, birb):
         self.client = client
         self.zoe = BotUtils.SENDER(self.client)
-
         self.birb = birb
+        self.watashi = self.birb.get_me()
 
 
     @commands.command()
     async def debug(self, ctx, args):
         try:
-            user = self.birb.get_user(screen_name=str(args))
-
+            #user = self.birb.get_user(screen_name=str(args))
+            user = self.birb.get_user(username=str(args))
             item = user
             await ctx.send(str(type(item)))
             await ctx.send(str(dir(item)))
