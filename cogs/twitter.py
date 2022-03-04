1fce8259ecee399f5e8489bef2aa3bd677db491e
KaitodotWav <78613734+KaitodotWav@users.noreply.github.com>
2022-03-05 00:38:35 +0800

    Update twitter.py

--- cogs/twitter.py
+++ cogs/twitter.py
@@ -131,6 +131,7 @@ class Twitter(commands.Cog):
                     med = tx["media"]
                     for m in med:
                         await ctx.send(str(m["media_url"]))
+                        await self.debug(ctx, med)
                 except:
                     tx = t.entities
                     await self.debug(ctx, tx)
